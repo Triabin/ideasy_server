02613609c198e5ca1457a14b251fe9098bfe0562
Triabin <a_bin333@163.com>
2024-07-10 02:25:24 +0800

    创建项目

---
+++ src/test/java/com/triabin/ideasy_server/IdeasyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triabin.ideasy_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeasyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
