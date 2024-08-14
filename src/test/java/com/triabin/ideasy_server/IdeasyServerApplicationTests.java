973d88855acca3af3b9364ac82160bcf21b29abc
Triabin <2676489081@qq.com>
2024-08-15 00:48:17 +0800

    BUG#001 DESC#解决日志字符集问题，删除测试类中的日志打印测试

--- src/test/java/com/triabin/ideasy_server/IdeasyServerApplicationTests.java
+++ src/test/java/com/triabin/ideasy_server/IdeasyServerApplicationTests.java
@@ -1,20 +1,12 @@
 package com.triabin.ideasy_server;
 
-import org.apache.logging.log4j.LogManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.apache.logging.log4j.Logger;
 
 @SpringBootTest
 class IdeasyServerApplicationTests {
 
-	private final Logger logger = LogManager.getLogger(IdeasyServerApplicationTests.class);
-
-	@Test
-	void contextLoads() {
-		logger.info("INFO级别日志{}", "占位符1");
-		logger.warn("WARN级别日志{}", "占位符1");
-		logger.error("ERROR级别日志", new Exception("异常"));
-	}
+    @Test
+    void contextLoads() {}
 
 }
